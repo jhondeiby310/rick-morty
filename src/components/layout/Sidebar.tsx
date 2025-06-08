d49d833ddbe6b84200af22ca7883f229e421c21f
jhon.montoya <john.montoya@cornercloud.io>
2025-06-08 15:40:17 -0500

    modify sidebar

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -8,7 +8,7 @@ import SeacrhAndFilters from "../filters/SearchAndFilters";
 export default function Sidebar() {
 
   return (
-    <aside className="relative max-h-screen w-full h-full md:w-[375px] bg-[#f3f4f636] px-4 pt-12 pb-4 border-r border-[#f3f4f636] flex flex-col">
+    <aside className="w-full md:w-[375px] h-screen flex flex-col bg-[#f3f4f636] px-4 pt-12 pb-4 border-r border-[#f3f4f636]">
       <SeacrhAndFilters />
       <div className="mt-6 px-1 flex-1 overflow-y-auto scrollbar-custom">
         <CharacterSidebarList />
